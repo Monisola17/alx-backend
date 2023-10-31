89bde49eac32769f6a4ebfcb9f21b66b1be8fb58
Monisola <khairaah@gmail.com>
2023-10-31 14:05:28 -0700

    Task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" 3-app module """
+from typing import Union
+from flask import Flask, request
+from flask_babel import Babel
+from routes.routes_3 import app_routes
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class for babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+app.register_blueprint(app_routes)
+
+
+@babel.localeselector
+def get_locale() -> Union[str, None]:
+    """ get locale
+    """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
